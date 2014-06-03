15319d8c705d29ebf67545b38bc55f7eeed21043
deanrussell <deanwagnerrussell@gmail.com>
2014-06-02 22:38:39 -0400

    tweaked jQuery

--- sfb.js
+++ sfb.js
@@ -2,8 +2,4 @@ $(document).ready(function(){
 	$('#dr').click(function(){
         $(this).effect('bounce', {times: 3}, 700);
     });
-    $('span').animate(function(){
-    	color: white;},
-    	500
-    );
 });
